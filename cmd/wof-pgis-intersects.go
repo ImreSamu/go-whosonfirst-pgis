e2042299e1db5bea0a44bd2e792d8bdd89065834
thisisaaronland <whosonfirst@mapzen.com>
2017-06-29 15:23:35 +0000

    stub out wof-pgis-intersects per issue #3

---
+++ cmd/wof-pgis-intersects.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"github.com/whosonfirst/go-whosonfirst-pgis/index"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	pgis_host := flag.String("pgis-host", "localhost", "The host of your PostgreSQL server.")
+	pgis_port := flag.Int("pgis-port", 5432, "The port of your PostgreSQL server.")
+	pgis_user := flag.String("pgis-user", "whosonfirst", "The name of your PostgreSQL user.")
+	pgis_pswd := flag.String("pgis-password", "", "The password of your PostgreSQL user.")
+	pgis_dbname := flag.String("pgis-database", "whosonfirst", "The name of your PostgreSQL database.")
+	pgis_maxconns := flag.Int("pgis-maxconns", 10, "The maximum number of connections to use with your PostgreSQL database.")
+
+	verbose := flag.Bool("verbose", false, "Be chatty about what's happening. This is automatically enabled if the -debug flag is set.")
+	debug := flag.Bool("debug", false, "Go through all the motions but don't actually index anything.")
+	strict := flag.Bool("strict", false, "Throw fatal errors rather than warning when certain conditions fails.")
+
+	flag.Parse()
+
+	if *debug {
+		*verbose = true
+	}
+
+	client, err := pgis.NewPgisClient(*pgis_host, *pgis_port, *pgis_user, *pgis_pswd, *pgis_dbname, *pgis_maxconns)
+
+	if err != nil {
+		log.Fatalf("failed to create PgisClient (%s:%d) because %v", *pgis_host, *pgis_port, err)
+	}
+
+	client.Verbose = *verbose
+	client.Debug = *debug
+
+}
